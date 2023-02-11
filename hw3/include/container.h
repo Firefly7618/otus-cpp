38a9ab2cdb9fdd54a3ddc8dc4c0c53676b8f43be
Yulia Dudysheva <firefly7618@gmail.com>
2023-02-11 15:36:09 +0300

    HW3: allocator alpha

---
+++ hw3/include/container.h
@@ -0,0 +1,83 @@
+#pragma once
+ 
+#include <cstdlib>
+#include <memory>
+ 
+ 
+template <typename T, typename Allocator = std::allocator<T>>
+class simple_vector
+{
+public:
+    simple_vector() = delete;
+    explicit simple_vector(size_t capacity) 
+    {
+        m_begin = m_allocator.allocate(capacity);
+
+        m_capacity = capacity;
+        m_size = 0;
+        m_end = m_begin;
+    }
+ 
+    ~simple_vector()
+    {
+        m_allocator.deallocate(m_begin, m_capacity);
+    }
+ 
+    T& operator[](size_t index)
+    {
+        return m_begin[index];
+    }
+ 
+    T* begin() noexcept
+    {
+        return m_begin;
+    }
+
+    T* end() noexcept
+    {
+        return m_end;
+    }
+ 
+    size_t size() const noexcept
+    {
+        return m_size;
+    }
+
+    size_t capacity() const noexcept
+    {
+         return m_capacity;
+    }
+
+    void push_back(const T& value)
+    {
+        if (m_size == m_capacity)
+        {
+            m_begin = m_allocator.allocate(1);
+            ++m_capacity;
+        }
+
+        m_allocator.construct(m_begin + m_size);
+
+        *(m_begin + m_size) = value;
+        ++m_size;
+ 
+        m_end = m_begin + m_size;
+    }
+ 
+private:
+    T* m_begin = nullptr;
+    T* m_end = nullptr;
+ 
+    size_t m_capacity = 0;
+    size_t m_size = 0;
+
+    Allocator m_allocator;
+  
+    void copy(T* source, size_t count, T* destination)
+    {
+        for (size_t iter = 0; iter < count; ++iter)
+        {
+            destination[iter] = source[iter];
+        }
+    }
+}; 
